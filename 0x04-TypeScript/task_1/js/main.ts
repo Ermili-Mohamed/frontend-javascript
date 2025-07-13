9b390345d8cdb59ce36adc7d71eed67ee6208aca
Med_User <mohamedermili5@gmail.com>
2025-07-13 14:21:03 +0100

    Printing teachers

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,47 @@
+interface Teacher {
+ readonly firstName: string;
+ readonly lastName: string;
+ fullTimeEmployee: boolean;
+ yearsOfExperience?: number;
+ location: string;
+ [key: string]: any; // allow extra properties
+}
+
+// Example usage:
+const teacher3: Teacher = {
+ firstName: 'John',
+ lastName: 'Doe',
+ fullTimeEmployee: false,
+ location: 'London',
+ contract: false,
+};
+
+console.log(teacher3);
+
+
+// Directors Interface
+
+interface Directors extends Teacher {
+ numberOfReports: number
+}
+
+const director1: Directors = {
+ firstName: 'John',
+ lastName: 'Doe',
+ location: 'London',
+ fullTimeEmployee: true,
+ numberOfReports: 17,
+};
+console.log(director1);
+
+
+// Printing teachers function
+
+interface printTeacherFunction {
+ (firstName: string, lastName: string): string
+
+}
+
+const printTeacher: printTeacherFunction = ((firstName, lastName) => {
+ return `${firstName.charAt(0)}. ${lastName}`
+})
